439bafeec994e7a073b08c46a1be077372af4fdb
Zidahn <inferno73189@yahoo.com>
2020-01-09 17:55:37 -0500

    Vue page intitial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h3>You're seeing this because Kevin is a beast!</h3>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
