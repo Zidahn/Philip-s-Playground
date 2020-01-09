34ea0c08c3be7e571851682e8da28760231bd715
Zidahn <inferno73189@yahoo.com>
2020-01-09 18:28:23 -0500

    typo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p><small>so much space for activities!!!</small></p>
+    <p><small>so much room for activities!!!</small></p>
   </div>
 </template>
